77c822c61cfbaab82fb88acfe66bf24c67d53ebd
NevercomeG <luisjavier2012@gmail.com>
2022-12-14 03:33:05 -0400

    1.1.3

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import "../styles/hangman.css";
+import "../styles/Hangman.css";
 import "bootstrap/dist/css/bootstrap.css";
 import Head from "next/head";
 import { NextIntlProvider } from "next-intl";
